d694820ef326b3866fee3f073377cf1e27508400
Karan Kadam <karank@outlook.com>
2022-05-18 11:32:35 +0930

    feat: 🎸 add file logging

--- util/logger.go
+++ util/logger.go
@@ -1,13 +1,36 @@
 package util
 
 // import zap logger
-import "go.uber.org/zap"
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
 // function to setup the logger
 func SetupLogger() *zap.Logger {
+	// configure the logger
+	config := zap.NewDevelopmentEncoderConfig()
+	config.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncodeDuration = zapcore.SecondsDurationEncoder
+	fileEncoder := zapcore.NewJSONEncoder(config)
+	consoleEncoder := zapcore.NewConsoleEncoder(config)
+
+	// log to file
+	logFile, err := os.OpenFile("log.json", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	writer := zapcore.AddSync(logFile)
+	defaultLogLevel := zap.DebugLevel
+	core := zapcore.NewTee(
+		zapcore.NewCore(fileEncoder, writer, defaultLogLevel),
+		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), defaultLogLevel),
+	)
+
 	// create the logger
-	logger, _ := zap.NewProduction()
-	defer logger.Sync()
+	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
 
 	// return the logger
 	return logger
