9af3f3bfd8b1936e8d2677e445bf5055957dff7c
Karan Kadam <karank@outlook.com>
2022-05-29 15:02:27 +0930

    feat: 🎸 ability to configure logger

--- util/logger.go
+++ util/logger.go
@@ -9,7 +9,7 @@ import (
 )
 
 // function to setup the logger
-func SetupLogger() *zap.Logger {
+func SetupLogger(logLevel zapcore.Level, fileMode bool) *zap.Logger {
 	// configure the logger
 	config := zap.NewDevelopmentEncoderConfig()
 	config.EncodeTime = zapcore.ISO8601TimeEncoder
@@ -17,17 +17,25 @@ func SetupLogger() *zap.Logger {
 	fileEncoder := zapcore.NewJSONEncoder(config)
 	consoleEncoder := zapcore.NewConsoleEncoder(config)
 
-	// log to file
-	logFile, err := os.OpenFile("log.json", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		panic(err)
+	var core zapcore.Core
+	if fileMode {
+		// log to file
+		logFile, err := os.OpenFile("log.json", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			panic(err)
+		}
+		writer := zapcore.AddSync(logFile)
+		defaultLogLevel := logLevel
+		core = zapcore.NewTee(
+			zapcore.NewCore(fileEncoder, writer, defaultLogLevel),
+			zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), defaultLogLevel),
+		)
+	} else {
+		defaultLogLevel := logLevel
+		core = zapcore.NewTee(
+			zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), defaultLogLevel),
+		)
 	}
-	writer := zapcore.AddSync(logFile)
-	defaultLogLevel := zap.DebugLevel
-	core := zapcore.NewTee(
-		zapcore.NewCore(fileEncoder, writer, defaultLogLevel),
-		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), defaultLogLevel),
-	)
 
 	// create the logger
 	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
