c214392789e9c518b3a957a7de0bc9425f42498e
Karan Kadam <karank@outlook.com>
2023-06-04 18:28:14 +0930

    fixes to docker compose

--- scripts/start_kramer.sh
+++ scripts/start_kramer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --rm --name kramerbot --network mongo-network --env-file=../token.env -p 3179:3179 kramerbot:latest
+docker run -d --rm --name kramerbot --network mongo-network --env-file=token.env -p 3179:3179 kramerbot:latest
