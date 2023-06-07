e82a9ac505906184f4863df5bf54d8e50c066f9a
Karan Kadam <karank@outlook.com>
2023-06-07 11:47:43 +0930

    refactor: 💡 clean up telegram functons, move to web ui

--- bot/botproc.go
+++ bot/botproc.go
@@ -4,14 +4,10 @@ import (
 	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	"github.com/intothevoid/kramerbot/util"
 	"go.uber.org/zap"
 )
 
 func (k *KramerBot) BotProc(updates tgbotapi.UpdatesChannel) {
-	// Keyword mode is used for registering keywords to watch
-	var keywordMode bool = false
-	var keywordClearMode bool = false
 	var announceMode bool = false
 
 	// keep watching updates channel
@@ -21,20 +17,6 @@ func (k *KramerBot) BotProc(updates tgbotapi.UpdatesChannel) {
 		}
 
 		k.Logger.Info("Received message", zap.String("text", update.Message.Text), zap.Int64("chatID", update.Message.Chat.ID))
-
-		// if keyword mode is on, process keyword
-		if keywordMode {
-			k.ProcessKeyword(update.Message.Chat, update.Message.Text)
-			keywordMode = false
-			continue
-		}
-
-		// if keyword clear mode is on, process clear keyword
-		if keywordClearMode {
-			k.ProcessClearKeyword(update.Message.Chat, update.Message.Text)
-			keywordClearMode = false
-			continue
-		}
 
 		if announceMode {
 			if k.verifyAdminPassword(update.Message.Text) {
@@ -47,79 +29,12 @@ func (k *KramerBot) BotProc(updates tgbotapi.UpdatesChannel) {
 			continue
 		}
 
-		// User asked for latest deals
-		if strings.Contains(strings.ToLower(update.Message.Text), "latest") {
-			k.SendLatestDeals(update.Message.Chat.ID, k.OzbScraper)
-			continue
-		}
-
-		// User asked to watch super deals i.e. 50+ upvotes within the hour
-		if strings.Contains(strings.ToLower(update.Message.Text), "watchsuper") {
-			k.WatchOzbSuperDeals(update.Message.Chat)
-			continue
-		}
-
-		// User asked to watch good OZB deals i.e. 25+ upvotes within the hour
-		if strings.Contains(strings.ToLower(update.Message.Text), "watchgood") {
-			k.WatchOzbGoodDeals(update.Message.Chat)
-			continue
-		}
-
-		// User asked to watch daily AMZ deals i.e. top daily deals with price drop > 20%
-		if strings.Contains(strings.ToLower(update.Message.Text), "amazondaily") {
-			k.WatchAmzDailyDeals(update.Message.Chat)
-			continue
-		}
-
-		// User asked to watch weekly AMZ deals i.e. top weekly deals with price drop > 20%
-		if strings.Contains(strings.ToLower(update.Message.Text), "amazonweekly") {
-			k.WatchAmzWeeklyDeals(update.Message.Chat)
-			continue
-		}
-
-		// User asked to watch specific keyword
-		if strings.Contains(strings.ToLower(update.Message.Text), "watchkeyword") {
-			if !keywordMode {
-				k.SendMessage(update.Message.Chat.ID, "Enter keyword to watch. Example: 'headphone' or 'sennheiser'")
-				keywordMode = true
-			}
-			continue
-		}
-
-		// User asked to clear specific watched keyword
-		if strings.Contains(strings.ToLower(update.Message.Text), "clearkeyword") {
-			if !keywordClearMode {
-				k.SendMessage(update.Message.Chat.ID, "Enter keyword to clear. Example: 'headphone' or 'sennheiser'")
-				keywordClearMode = true
-			}
-			continue
-		}
-
-		// User asked to clear all watched keywords
-		if strings.Contains(strings.ToLower(update.Message.Text), "clearallkeywords") {
-			k.ProcessClearAllKeywords(update.Message.Chat)
-			continue
-		}
-
-		// User requested current status
-		if strings.Contains(strings.ToLower(update.Message.Text), "status") {
-			k.SendStatus(update.Message.Chat)
-			continue
-		}
-
 		// User requested admin function - announce
 		if strings.Contains(strings.ToLower(update.Message.Text), "announcement") {
 			if !announceMode {
 				k.SendMessage(update.Message.Chat.ID, "Enter admin password and announcement in format password:announcement")
 				announceMode = true
 			}
-			continue
-		}
-
-		// User asked for a kramerism
-		if strings.Contains(strings.ToLower(update.Message.Text), "kramerism") {
-			kramerism := util.GetKramerism()
-			k.SendMessage(update.Message.Chat.ID, kramerism)
 			continue
 		}
 
